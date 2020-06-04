c1de20e1b9be2abe15cc4cd5c08d62f2d3144277
hanfanw <hanfanw@gmail.com>
2020-06-04 12:38:13 -0400

    routing demo

--- routing-demo/src/app/app.module.ts
+++ routing-demo/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+// import { DepartmentListComponent } from './department-list/department-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    PageNotFoundComponent
+    // EmployeeListComponent,
+    // DepartmentListComponent
   ],
   imports: [
     BrowserModule,
