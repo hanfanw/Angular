ebcc126d2275f8cf73386551b948274b11fe8255
hanfanw <hanfanw@gmail.com>
2020-08-10 10:30:49 -0400

    41

--- routing-demo/src/app/department-detail/department-detail.component.ts
+++ routing-demo/src/app/department-detail/department-detail.component.ts
@@ -1,28 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import {ClassGetter} from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { ClassGetter } from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'app-department-detail',
+  selector: "app-department-detail",
   template: `
-    <h3>
-      You selected department with id = {{ departmentId }}
-    </h3>
-    <a (click)="goPrevious()">Previous</a>
+    <h3>You selected department with id = {{ departmentId }}</h3>
+    <a (click)="goPrevious()">Previous</a>&nbsp;&nbsp;
     <a (click)="goNext()">Next</a>
 
     <div>
       <button (click)="gotoDepartments()">Back</button>
     </div>
   `,
-  styles: []
+  styles: [],
 })
 export class DepartmentDetailComponent implements OnInit {
-
   public departmentId;
 
-  constructor(private route: ActivatedRoute, private router: Router) {
-  }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     // tslint:disable-next-line:radix
@@ -31,25 +27,27 @@ export class DepartmentDetailComponent implements OnInit {
     this.route.paramMap.subscribe((params: ParamMap) => {
       console.log(params);
       // tslint:disable-next-line:radix
-      const id = parseInt(params.get('id'));
+      const id = parseInt(params.get("id"));
       this.departmentId = id;
     });
   }
 
   goPrevious() {
     const previousId = this.departmentId - 1;
-    this.router.navigate(['/departments', previousId]);
+    this.router.navigate(["/departments", previousId]);
   }
 
   goNext() {
     const nextId = this.departmentId + 1;
-    this.router.navigate(['departments', nextId]);
+    this.router.navigate(["departments", nextId]);
   }
 
   gotoDepartments() {
     const selectedId = this.departmentId ? this.departmentId : null;
-    this.router.navigate(['/departments', {id: selectedId, test: 'testValue'}]);
+    this.router.navigate([
+      "/departments",
+      { id: selectedId, test: "testValue" },
+    ]);
     // Note:  {id: selectedId, test: 'testValue' }  is optional route parameters
   }
-
 }
